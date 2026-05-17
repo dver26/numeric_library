d7c67d6827d7ad59e228066ceeda76c57c38c5cd
dver26 <fercorvictor@gmail.com>
2026-05-17 14:55:45 +0200

    modified_euler_method

--- ode_methods/euler.py
+++ ode_methods/euler.py
@@ -3,6 +3,15 @@ def euler_method(a, b, n, y0, f):
     y = [y0]
 
     for i in range(1, n+1):
-        y.append(y[i-1] + h*f(a+(i-1)*h), y[i-1])
+        y.append(y[i-1] + h*f(a+(i-1)*h, y[i-1]))
+
+    return y
+
+def modified_euler_method(a, b, n, y0, f):
+    h = (b-a)/n
+    y = [y0]
+
+    for i in range(1, n+1):
+        y.append(y[i-1] + (h/2)*(f(a + (i-1)*h, y[i-1]) + f(a+i*h, y[i-1]+h*f(a+(i-1)*h, y[i-1]))))
 
     return y
